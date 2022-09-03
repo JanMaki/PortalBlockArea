7204b05add1459ecb8da85bc0f8abc9b7173cb7b
JanMaki <44093615+JanMaki@users.noreply.github.com>
2022-09-03 09:36:00 +0900

    Add files via upload

---
+++ PortalBlockArea.cs
@@ -0,0 +1,20 @@
+﻿using UdonSharp;
+using UnityEngine;
+
+namespace PortalBlockArea
+{
+    /// <summary>
+    /// ポータル設置不可エリア
+    /// </summary>
+    public class PortalBlockArea : UdonSharpBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            //当たったオブジェクトがポータルじゃないかを確認
+            if (!other.gameObject.name.Contains("Portals/PortalInternalDynamic")) return;
+
+            //削除
+            Destroy(other.gameObject);
+        }
+    }
+}
