1eca42db14ccbb4e5f8671c72975d44e032177f9
JanMaki <44093615+JanMaki@users.noreply.github.com>
2024-09-13 23:16:23 +0900

    Update PortalBlockArea.cs

--- PortalBlockArea.cs
+++ PortalBlockArea.cs
@@ -1,4 +1,4 @@
-﻿using UdonSharp;
+using UdonSharp;
 using UnityEngine;
 
 namespace PortalBlockArea
@@ -6,13 +6,15 @@ namespace PortalBlockArea
     /// <summary>
     /// ポータル設置不可エリア
     /// </summary>
+    [RequireComponent(typeof(Collider))]
     public class PortalBlockArea : UdonSharpBehaviour
     {
         private void OnTriggerEnter(Collider other)
         {
             //当たったオブジェクトがポータルじゃないかを確認
-            if (!other.gameObject.name.Contains("Portals/PortalInternalDynamic")) return;
-
+            if (!other.gameObject.name.Contains("PortalInternal") 
+                && !other.gameObject.name.Contains("PortalSelector")) return;
+            
             //削除
             Destroy(other.gameObject);
         }
